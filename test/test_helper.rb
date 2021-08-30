9e5c1e602647c1cb172237937dcd29c1bff35bc1
kaur0507 <karolina.urbaniak@akra.net>
2021-08-30 16:36:42 +0200

    last changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
 end
