2c6a404c2bc8853ffed07a507b63ea86d9cb8884
kaur0507 <karolina.urbaniak@akra.net>
2021-09-01 16:39:07 +0200

    followers and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,34 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+
   def show
     user = User.find(params[:id])
     @tweeets = user.tweeets
     @user_id = user.id
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   # Follows a user.
   def follow(other_user)
-    user = User.find(params[:id])
     active_relationships.create(followed_id: other_user.id)
   end
 
   # Unfollows a user.
   def unfollow(other_user)
-    user = User.find(params[:id])
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
