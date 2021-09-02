62901a25fc74390b3e0510bd4ecde2f559f118b3
kaur0507 <karolina.urbaniak@akra.net>
2021-09-02 15:56:58 +0200

    ActiveAdmin

---
+++ app/admin/tweeets.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Tweeet do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :tweeet, :user_id, :image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:tweeet, :user_id, :image]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
